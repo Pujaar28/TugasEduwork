ab27e0905f5f74195f0644af7cb7ff5bfed3329f
DESKTOP-52KS6G7\akhma <adityamazter@gmail.com>
2023-12-05 13:41:24 +0700

    Cleaning code

---
+++ jscript.js
@@ -0,0 +1,19 @@
+    let x = "10"+10+10;
+    let y = 10+10+"10";
+    let z= 12;
+    const person = {firstName: "John",lastName:"Doe"};
+        alert(person.firstName + " Please extract information from Object Person")
+    console.log(y);    
+    console.log(x);
+        
+    console.log(x+y);
+    console.log(x+z);
+
+        console.log("Saya belajar Javascript");
+        document.write("Hello World!");
+
+        function myFunction(){
+            alert("Hello WOrld");
+        }
+        myFunction();
+
