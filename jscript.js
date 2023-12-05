6c0afd3fc01668f7b8f5f3387f1927577489fd6e
DESKTOP-52KS6G7\akhma <adityamazter@gmail.com>
2023-12-05 13:58:33 +0700

    add form condition and add new javascript condition if else

--- jscript.js
+++ jscript.js
@@ -16,4 +16,17 @@
             alert("Hello WOrld");
         }
         myFunction();
+function conditionOne(){
+    let nilaiOne = document.getElementById("nilaiPertama").value;
+    let nilaiTwo = document.getElementById("nilaiKedua").value;
 
+    if (nilaiOne > nilaiTwo){
+        alert("Nilai pertama lebih besar dari pada nilai kedua");
+    }
+    else if (nilaiOne < nilaiTwo){
+        alert("Nilai kedua lebih besar dari pada nilai Pertama");    
+    }else{
+        alert("Kedua nilai memiliki nilai yang sama")
+    }
+}
+    
